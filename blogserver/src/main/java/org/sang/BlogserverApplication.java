5527fa75dc46ec47b5d5b66e585c1e4f5ebe9cb0
upchr <5593525+upchr@user.noreply.gitee.com>
2020-10-12 10:29:19 +0800

    deploy test

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -3,7 +3,9 @@ package org.sang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+/**
+ *  进入网址localhost:8089/index.html
+ */
 @SpringBootApplication
 @EnableScheduling//开启定时任务支持
 public class BlogserverApplication {
